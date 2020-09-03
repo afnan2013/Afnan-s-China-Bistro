6cd7658f54a371f621fae390fc834b1f8609ff32
kucse160235 <m.afnan2016@gmail.com>
2020-09-04 05:57:00 +0600

    Deletion of single-category.html file and make this dynamic
    1. Specials tile in home page is now working perfectly (issue fixed)
    2. menu items for each category parsed from menuitemsUrl in scripts.js
       file
    3. Category page image loading issue is solved.
    4. Now the website is completely dynamic

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -26,6 +26,9 @@ $(function () {	// Same as document.addEventListener("DOMContentLoaded"...
 	var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 	var categoriesTitleHtml = "snippets/categories-title-snippet.html"; 
 	var categoryHtml = "snippets/category-snippet.html";
+	var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+	var menuItemTitleHtml = "snippets/menu-item-title-snippet.html";
+	var menuItemHtml = "snippets/menu-item-snippet.html";
 
 	// Convenience function for inserting innerHTML for 'select'
 	function insertHtml(selector, html) {
@@ -102,13 +105,79 @@ $(function () {	// Same as document.addEventListener("DOMContentLoaded"...
 		return finalHtml;
 	}
 
-
-	abc.loadMenuItems = function () {
+	// Load the menu items view of a particular category 
+	abc.loadMenuItems = function (categoryShortName) {
 		showLoader("#main-content");
-
-		$ajaxUtils.sendGetRequest()
+		$ajaxUtils.sendGetRequest(menuItemsUrl+categoryShortName, buildAndShowMenuItemsHTML)
 	};
 
+	function buildAndShowMenuItemsHTML(menuItemsRes) {
+		//console.log(menuItemsRes);
+		$ajaxUtils.sendGetRequest(menuItemTitleHtml, function (menuItemTitleHtmlRes) {
+			//console.log(menuItemTitleHtmlRes);
+			$ajaxUtils.sendGetRequest(menuItemHtml, function (menuItemHtmlRes) {
+				//console.log(menuItemHtmlRes);
+				var menuItemView = buildMenuItemsViewHtml(menuItemsRes, menuItemTitleHtmlRes, menuItemHtmlRes);
+				console.log(menuItemView);
+				insertHtml("#main-content", menuItemView);
+			}, false);
+		}, false);
+	}
+
+
+	function buildMenuItemsViewHtml(menuItemsRes, menuItemTitleHtmlRes, menuItemHtmlRes) {
+		var finalHtml = menuItemTitleHtmlRes;
+		finalHtml = insertProperty(finalHtml, "name", menuItemsRes.category.name);
+		finalHtml = insertProperty(finalHtml, "special_instructions", menuItemsRes.category.special_instructions);
+
+		finalHtml += '<section class="row">';
+
+		var menuItems = menuItemsRes.menu_items;
+		var cat_short_name = menuItemsRes.category.short_name;
+
+		for (var i = 0; i < menuItems.length; i++) {
+			finalHtml += menuItemHtmlRes;
+
+			finalHtml = insertProperty(finalHtml, "short_name", menuItems[i].short_name);
+			finalHtml = insertProperty(finalHtml, "cat_short_name", cat_short_name);
+			finalHtml = insertItemPrice(finalHtml, "price_small", menuItems[i].price_small);
+	    finalHtml = insertItemPortionName(finalHtml, "small_portion_name", menuItems[i].small_portion_name);
+	    finalHtml = insertItemPrice(finalHtml, "price_large", menuItems[i].price_large);
+	    finalHtml = insertItemPortionName(finalHtml, "large_portion_name", menuItems[i].large_portion_name);
+			finalHtml = insertProperty(finalHtml, "name", menuItems[i].name);
+			finalHtml = insertProperty(finalHtml, "description", menuItems[i].description);
+
+			if (i%2 != 0){
+				finalHtml += "<div class='clearfix visible-lg-block visible-md-block'></div>";
+			}
+		}
+
+		finalHtml += '</section>';
+
+		return finalHtml;
+	}
+
+	function insertItemPrice(html, pricePropName, priceValue) {
+		// If not specified, replace with empty string
+	  if (!priceValue) {
+	    return insertProperty(html, pricePropName, "");;
+	  }
+
+	  priceValue = "$" + priceValue.toFixed(2);
+  	html = insertProperty(html, pricePropName, priceValue);
+  	return html;
+
+	}
+
+	function insertItemPortionName(html, portionPropName, portionValue) {
+		// If not specified, replace with empty string
+	  if (!portionValue) {
+	    return insertProperty(html, portionPropName, "");;
+	  }
+	  portionValue = "(" + portionValue + ")";
+	  html = insertProperty(html, portionPropName, portionValue);
+	  return html;
+	}
 	global.$abc = abc;
 
 })(window);
