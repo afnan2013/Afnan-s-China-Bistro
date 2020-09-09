e73215e66e28a34d589af7ca59e90a9d8226e17d
kucse160235 <m.afnan2016@gmail.com>
2020-09-09 22:53:35 +0600

    Solution updated of Assignment 1: add optional things

--- ang-module1-solution/app.js
+++ ang-module1-solution/app.js
@@ -1,26 +1,35 @@
 (function (global) {
 
 
-angular.module("LunchCheckApp", [])
+angular.module("LunchCheck", [])
 	.controller("LunchCheckController", LunchCheckController);
 
 	LunchCheckController.$inject = ['$scope'];
 	function LunchCheckController($scope) {
 		$scope.lunch = "";
-		$scope.message = "The message will be here";
+		$scope.message = "";
 		$scope.checkTooMuch = function () {
 			var lunch_menu = $scope.lunch;
 			if (lunch_menu != ""){
 				lunch_menu = lunch_menu.split(',');
-				if (lunch_menu.length > 3){
+				var count = 0;
+				for (var i =0; i< lunch_menu.length; i++){
+					if (lunch_menu[i].trim() != ""){
+						count++;
+					}
+				}
+				if (count > 3){
 					$scope.message = "Too much!";
+					$scope.color = "red";
 				}
 				else{
-					$scope.message = "Enjoy!"
+					$scope.message = "Enjoy!";
+					$scope.color = "green";
 				}
 			}
 			else{
 				$scope.message = "Please enter data first!"
+				$scope.color = "red";
 			}
 		}
 	}
