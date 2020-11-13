fc27765c4b2dd479f3684c931f505116c627ec94
kucse160235 <m.afnan2016@gmail.com>
2020-11-14 02:44:55 +0600

    Uploading the Particle-js Demo Project

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 $(function () {	// Same as document.addEventListener("DOMContentLoaded"...	
 	// Same as document.querySelector("#navbarToggle").addEventListener("blur",...
 	$("#navbarToggle").blur(function(event) {
@@ -200,4 +203,3 @@ $(function () {	// Same as document.addEventListener("DOMContentLoaded"...
 	global.$abc = abc;
 
 })(window);
-
